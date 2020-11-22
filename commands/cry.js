eae48e8561a90e4e12bc133e51d0ffe71ef4793d
Glitch (fine-turquoise-headline) <none>
2020-11-22 20:09:08 +0000

    ✂️🗺 Checkpoint ./commands/kiss.js:23542682/1009 ./commands/cry.js :23542682/823 ./commands/hug.js copy:23542682/820 ./commands/lick.js:23542682/847 ./commands/cry.js:23542682/870

---
+++ commands/cry.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const request = require('request');
+const fetch = require("node-fetch");
+
+module.exports = {
+    name: 'cry',
+    description: 'Some command description here.',
+    guildOnly: false,
+    nsfw: false,
+
+    execute(bot, message, args) {
+        const user = message.mentions.users.first() || message.author;
+        if (!user) return message.channel.send("**Could not find the user**");
+
+        request({ url: "https://waifu.pics/api/sfw/cry", json: true }, function(error, response, body) {
+                let henEmbed = new Discord.RichEmbed()
+                    .setColor("#03e3fc")
+                    .setAuthor(message.author.username + " started crying blame on " + user.username)
+                    .setImage(body.url)
+            message.channel.send(henEmbed);
+        });
+
+    }
+};
---
+++ commands/lick.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const request = require('request');
+const fetch = require("node-fetch");
+
+module.exports = {
+    name: 'lick',
+    description: 'Some command description here.',
+    guildOnly: false,
+    nsfw: false,
+
+    execute(bot, message, args) {
+        const user = message.mentions.users.first() || message.author;
+        if (!user) return message.channel.send("**Could not find the user**");
+
+        request({ url: "https://waifu.pics/api/sfw/lick", json: true }, function(error, response, body) {
+                let henEmbed = new Discord.RichEmbed()
+                    .setColor("#03e3fc")
+                    .setAuthor(message.author.username + " has licked " + user.username)
+                    .setImage(body.url)
+            message.channel.send(henEmbed);
+        });
+
+    }
+};
