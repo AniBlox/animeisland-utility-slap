aa6d822a45ac2f1c517a2900201322377f864012
Glitch (fine-turquoise-headline) <none>
2020-11-22 20:32:29 +0000

    🌛🌺 Checkpoint ./commands/happy.js:23542682/4 ./commands/wink.js:23542682/860 ./commands/poke.js:23542682/848 ./commands/dance.js:23542682/880 ./commands/cringe.js:23542682/872 ./commands/blush.js:23542682/876

--- commands/poke.js
+++ commands/poke.js
@@ -3,7 +3,7 @@ const request = require('request');
 const fetch = require("node-fetch");
 
 module.exports = {
-    name: 'happy',
+    name: 'poke',
     description: 'Some command description here.',
     guildOnly: false,
     nsfw: false,
@@ -12,10 +12,10 @@ module.exports = {
         const user = message.mentions.users.first() || message.author;
         if (!user) return message.channel.send("**Could not find the user**");
 
-        request({ url: "https://waifu.pics/api/sfw/happy", json: true }, function(error, response, body) {
+        request({ url: "https://waifu.pics/api/sfw/poke", json: true }, function(error, response, body) {
                 let henEmbed = new Discord.RichEmbed()
                     .setColor("#03e3fc")
-                    .setAuthor(message.author.username + " is happy...Is very happy.. ")
+                    .setAuthor(message.author.username + " has poked " + user.username)
                     .setImage(body.url)
             message.channel.send(henEmbed);
         });
