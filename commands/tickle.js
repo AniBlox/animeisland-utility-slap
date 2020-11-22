899bea6dae468ae00db2c12e8757dc5f09c7638a
Glitch (fine-turquoise-headline) <none>
2020-11-22 19:11:32 +0000

    🌁💡 Checkpoint ./commands/tickle.js:23542682/74

--- commands/tickle.js
+++ commands/tickle.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const request = require('request');
+const fetch = require("node-fetch");
 
 module.exports = {
     name: 'tickle',
@@ -11,7 +12,7 @@ module.exports = {
         const user = message.mentions.users.first() || message.author;
         if (!user) return message.channel.send("**Could not find the user**");
 
-        request({ url: "https://waifu.pics/api/sfw/hug", json: true }, function(error, response, body) {
+        request({ url: "https://waifu.pics/api/sfw/hug , https://nekos.life/api/v2/img/smug", json: true }, function(error, response, body) {
                 let henEmbed = new Discord.RichEmbed()
                     .setColor("#03e3fc")
                     .setAuthor(message.author.username + " tickles " + user.username)
