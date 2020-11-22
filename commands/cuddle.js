62ded16e6396d13ac0113fefb9d33c1c334380de
Glitch (fine-turquoise-headline) <none>
2020-11-22 18:25:16 +0000

    🎟🌱 Checkpoint ./commands/cuddle.js:23542682/178

--- commands/cuddle.js
+++ commands/cuddle.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const request = require('request');
+const fetch = require("node-fetch");
 
 module.exports = {
     name: 'cuddle',
@@ -11,7 +12,7 @@ module.exports = {
         const user = message.mentions.users.first() || message.author;
         if (!user) return message.channel.send("**Could not find the user**");
 
-        request({ url: "https://nekos.life/api/v2/img/cuddle", json: true }, function(error, response, body) {
+        request({ url: "https://waifu.pics/api/sfw/slap", json: true }, function(error, response, body) {
                 let henEmbed = new Discord.RichEmbed()
                     .setColor("#fcc203")
                     .setAuthor(message.author.username + " **cuddles** ")
