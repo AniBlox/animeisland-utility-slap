c19972913e79d5051b03c35bfc674ddf52a05b5a
Glitch (fine-turquoise-headline) <none>
2020-11-23 07:24:33 +0000

    ⚓️🌃 Checkpoint ./commands/cuddle.js:23542682/1103 ./commands/OwO.js:23542682/865 ./commands/fact.js:23542682/2182

---
+++ commands/cuddle.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const request = require('request');
+const fetch = require("node-fetch");
+
+module.exports = {
+    name: 'cuddle',
+    description: 'Some command description here.',
+    guildOnly: false,
+    nsfw: false,
+
+    execute(bot, message, args) {
+        const user = message.mentions.users.first() || message.author;
+        if (!user) return message.channel.send("**Could not find the user**");
+
+        request({ url: "https://nekos.life/api/v2/img/cuddle", json: true }, function(error, response, body) {
+                let henEmbed = new Discord.RichEmbed()
+                    .setColor("#03e3fc")
+                    .setAuthor(message.author.username + " cuddles " + user.username)
+                    .setImage(body.url)
+            message.channel.send(henEmbed);
+        });
+
+    }
+};
