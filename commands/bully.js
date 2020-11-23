1c1906edabd07f482e9b98871fbfeda6b3e42c34
Glitch (fine-turquoise-headline) <none>
2020-11-23 07:13:18 +0000

    ⛺️🌂 Checkpoint ./commands/dance.js:23542682/1 ./commands/bully.js:23542682/848

--- commands/bully.js
+++ commands/bully.js
@@ -3,7 +3,7 @@ const request = require('request');
 const fetch = require("node-fetch");
 
 module.exports = {
-    name: 'dance',
+    name: 'bully',
     description: 'Some command description here.',
     guildOnly: false,
     nsfw: false,
@@ -12,10 +12,10 @@ module.exports = {
         const user = message.mentions.users.first() || message.author;
         if (!user) return message.channel.send("**Could not find the user**");
 
-        request({ url: "https://waifu.pics/api/sfw/dance", json: true }, function(error, response, body) {
+        request({ url: "https://waifu.pics/api/sfw/bully", json: true }, function(error, response, body) {
                 let henEmbed = new Discord.RichEmbed()
                     .setColor("#03e3fc")
-                    .setAuthor(message.author.username + " dances happily ")
+                    .setAuthor(message.author.username + " is bullying " + user.username)
                     .setImage(body.url)
             message.channel.send(henEmbed);
         });
